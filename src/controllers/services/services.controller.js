35d2f37693b8d0a7baeeef387152d6ca92469570
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-05-05 12:33:50 +0330

    Update

--- src/controllers/services/services.controller.js
+++ src/controllers/services/services.controller.js
@@ -1,18 +1,24 @@
 import pm2 from "pm2";
 
 export const ALL = async (req, res) => {
+  const slug = "tfasoft";
+
   pm2.list((error, list) => {
     if (error) {
       res.status(200).send({ message: error.message });
     }
 
-    res.status(200).send(
-      list.sort((a, b) => {
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-        return 0;
-      })
-    );
+    const current = list.filter((item) => item.name.includes(slug));
+
+    current.map((item) => (item.name = item.name.replaceAll(`${slug}-`, "")));
+
+    current.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+
+    res.status(200).send(current);
   });
 };
 
